f181aac2cf238091ec70e22d0ced573b29928637
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-06-14 19:07:36 +0300

    Create weightIATexample.js

---
+++ IAT/qualtrics/weightIATexample.js
@@ -0,0 +1,114 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat8.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+    var baseURL = 'https://baranan.github.io/minno-tasks/images/';
+    var posWords = API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure', 
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
+    var negWords = API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
+    
+    return iatExtension({
+        category1 : {
+            name : 'Fat people', //Will appear in the data.
+            title : {
+                media : {word : 'Fat people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'fatman1.jpg'},
+                {image: 'fatman3.jpg'},
+                {image: 'fatman4.jpg'},
+				{image: 'fatman5.jpg'},
+                {image: 'fatman6.jpg'},                 
+				{image: 'fatwoman1.jpg'},
+                {image: 'fatwoman2.jpg'},
+                {image: 'fatwoman3.jpg'},
+                {image: 'fatwoman4.jpg'},
+                {image: 'fatwoman6.jpg'}     
+    	    ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 :    {
+            name : 'Thin people', //Will appear in the data.
+            title : {
+                media : {word : 'Thin people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'thinman1.jpg'},
+                {image: 'thinman2.jpg'},
+                {image: 'thinman3.jpg'},
+				        {image: 'thinman5.jpg'},
+                {image: 'thinman6.jpg'},                 
+				        {image: 'thinwoman1.jpg'},
+                {image: 'thinwoman2.jpg'},
+                {image: 'thinwoman3.jpg'},
+				        {image: 'thinwoman5.jpg'},
+                {image: 'thinwoman6.jpg'}  
+            ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+		attribute1 :
+		{
+			name : 'Bad words',
+			title : {
+				media : {word : 'Bad words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: negWords[0]},
+				{word: negWords[1]},
+				{word: negWords[2]},
+				{word: negWords[3]},
+				{word: negWords[4]},
+				{word: negWords[5]},
+				{word: negWords[6]},
+				{word: negWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Good words',
+			title : {
+				media : {word : 'Good words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: posWords[0]},
+				{word: posWords[1]},
+				{word: posWords[2]},
+				{word: posWords[3]},
+				{word: posWords[4]},
+				{word: posWords[5]},
+				{word: posWords[6]},
+				{word: posWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+        base_url : {//Where are your images at?
+            image : baseURL
+        }
+    });
+});
