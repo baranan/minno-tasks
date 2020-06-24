8e6d00de4ed03028a9b132be06cd25b0b02ae539
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-06-24 12:18:44 +0300

    Update qualtricsST-IAT.js

--- stiat/qualtricsST-IAT.js
+++ stiat/qualtricsST-IAT.js
@@ -1,47 +1,66 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/stiat6.js'], function(APIConstructor, iatExtension){
-  var API = new APIConstructor();
-
-	return iatExtension({
-		category1 : {
-			name : 'Black people', //Will appear in the data.
-			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
+define(['pipAPI', 'https://elinorbengayev.github.io/STIAT.js'], function(APIConstructor, stiatExtension){
+	var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Black people', //Will appear in the data.
+		    title : {
+		      media : {word : 'Black people'}, //Name of the category presented in the task.
+		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    	{image : 'black1.jpg'}, 
     			{image : 'black2.jpg'}, 
     			{image : 'black3.jpg'}, 
     			{image : 'black4.jpg'}, 
     			{image : 'black5.jpg'}, 
     			{image : 'black6.jpg'}
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'White people', //Will appear in the data.
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#31b404','font-size':'3em'}
+		  },	
+
+  		attribute1 : 
+			{
+			name : 'Unpleasant', //Attribute label
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
+				media : {word : 'Negative'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
 			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
+			media : [ //Stimuli
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+		attribute2 : 
+			{
+			name : 'Pleasant', //Attribute label
+			title : {
+				media : {word : 'Positive'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
 
-		base_url : {//Where are your images at?
-			//image : 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/images/'
-      image : 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/images/'
-		} 
-	});
-});
-
+  base_url : {//Where are your images at?
+    image : 'https://baranan.github.io/minno-tasks/images/'
+  }}
+  );
+  });
