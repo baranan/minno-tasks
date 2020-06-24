9b9340ed84ea1a43f763291c070b60110b5ed822
galmaimon <57755781+galmaimon@users.noreply.github.com>
2020-06-24 13:59:58 +0300

    Update exampleBIATrace.js

--- BIAT/exampleBIATrace.js
+++ BIAT/exampleBIATrace.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://galmaimon.github.io/BIAT.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/biat7.js'], function(APIConstructor, iatExtension){
   var API = new APIConstructor();
 
 	
