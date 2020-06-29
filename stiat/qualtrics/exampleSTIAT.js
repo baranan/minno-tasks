86f95260aefd6e48958a823f152b6f7004b60f1c
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-06-29 21:17:21 +0300

    Update and rename IAT/qualtrics/exampleSTIAT.js to stiat/qualtrics/exampleSTIAT.js

--- stiat/qualtrics/exampleSTIAT.js
+++ stiat/qualtrics/exampleSTIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
