aa28644ef5a34ba3aca6341b9b41b51d69c7e3f0
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-07-25 17:55:03 +0300

    Create pos_neg_amp.js

---
+++ amp/qualtrics/pos_neg_amp.js
@@ -0,0 +1,67 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+//https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/amp4.js
+	var API = new APIConstructor();
+	var global = API.getGlobal();
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'positive',  //Will be used in the user feedback 
+				nameForLogging : 'positive', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+                    {image: 'pos1.jpg'},
+                    {image: 'pos2.jpg'},
+                    {image: 'pos3.jpg'},
+                    {image: 'pos4.jpg'},
+                    {image: 'pos5.jpg'},
+                    {image: 'pos6.jpg'},
+                    {image: 'pos7.jpg'},
+                    {image: 'pos8.jpg'},
+                    {word: 'Paradise'},
+                    {word: 'Pleasure'},
+                    {word: 'Cheer'},
+                    {word: 'Splendid'},
+                    {word: 'Love'},
+                    {word: 'Smile'},
+                    {word: 'Friend'},
+                    {word: 'Enjoy'}
+        		]
+			}, 
+			{
+				nameForFeedback : 'negative',  //Will be used in the user feedback 
+				nameForLogging : 'negative', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+                    {image: 'neg1.jpg'},
+                    {image: 'neg2.jpg'},
+                    {image: 'neg3.jpg'},
+                    {image: 'neg4.jpg'},
+                    {image: 'neg5.jpg'},
+                    {image: 'neg6.jpg'},
+                    {image: 'neg7.jpg'},
+                    {image: 'neg8.jpg'},
+                    {word: 'Bomb'},
+                    {word: 'Abuse'},
+                    {word: 'Sadness'},
+                    {word: 'Pain'},
+                    {word: 'Grief'},
+                    {word: 'Ugly'},
+                    {word: 'Poison'},
+                    {word: 'Stink'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			//mediaArray : [{word : 'Table'}, {word : 'Chair'}]
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		}
+	});
+});
