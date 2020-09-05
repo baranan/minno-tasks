fa50dc09d7c72b4209c7603741b857d2babc2493
baranan <baranan@gmail.com>
2020-09-05 09:17:37 +0300

    Update demosexuality0002.js

--- IAT/qualtrics/demosexuality0002.js
+++ IAT/qualtrics/demosexuality0002.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 	var posWords = API.shuffle([
