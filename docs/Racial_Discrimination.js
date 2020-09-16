6b2ea4cbb96624f388ba3f2b8152c6606e6dbf1f
baranan <baranan@gmail.com>
2020-09-16 06:47:04 +0300

    Update Racial_Discrimination.js

--- docs/Racial_Discrimination.js
+++ docs/Racial_Discrimination.js
@@ -1,5 +1,5 @@
-//define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/qiattest.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+//define(['pipAPI', 'https://baranan.github.io/minno-tasks/qiattest.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
