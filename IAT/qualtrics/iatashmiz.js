a823e6506f38cb40e5de2c61bce8d4be32f68460
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-07-27 15:28:06 +0300

    Update iatashmiz.js

--- IAT/qualtrics/iatashmiz.js
+++ IAT/qualtrics/iatashmiz.js
@@ -1,4 +1,4 @@
-define(['https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat10.js'], function(iatExtension){
+define(['https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(iatExtension){
 
     return iatExtension({
         category1 : {
