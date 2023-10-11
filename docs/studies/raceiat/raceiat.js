83069f8fde70409ed827537b0fd862dd4cea489c
baranan <baranan@tauex.tau.ac.il>
2023-10-11 15:58:56 +0100

    Add all the study files

---
+++ docs/studies/raceiat/raceiat.js
@@ -0,0 +1,88 @@
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
+    let API = new APIConstructor();
+    let global = API.getGlobal();
+
+    return iatExtension({
+        category1 : {
+            name : global.blackLabels, //Will appear in the data.
+            title : {
+                media : {word : global.blackLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
+                {image: 'bm2_nc.jpg'},
+                {image: 'bm3_nc.jpg'},
+                {image: 'bf1_nc.jpg'},
+                {image: 'bf2_nc.jpg'},                 
+                {image: 'bf3_nc.jpg'}     
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 : {
+            name : global.whiteLabels, //Will appear in the data.
+            title : {
+                media : {word : global.whiteLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+                {image: 'wf1_nc.jpg'},
+                {image: 'wf2_nc.jpg'},
+                {image: 'wf3_nc.jpg'}
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+        attribute1 : {
+            name : 'Bad words',
+            title : {
+                media : {word : 'Bad words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.negWords[0]},
+                {word: global.negWords[1]},
+                {word: global.negWords[2]},
+                {word: global.negWords[3]},
+                {word: global.negWords[4]},
+                {word: global.negWords[5]},
+                {word: global.negWords[6]},
+                {word: global.negWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good words',
+            title : {
+                media : {word : 'Good words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.posWords[0]},
+                {word: global.posWords[1]},
+                {word: global.posWords[2]},
+                {word: global.posWords[3]},
+                {word: global.posWords[4]},
+                {word: global.posWords[5]},
+                {word: global.posWords[6]},
+                {word: global.posWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        base_url : {//Where are your images at?
+            image : global.baseURL
+        },
+        isTouch : global.$isTouch
+    });
+});
+
