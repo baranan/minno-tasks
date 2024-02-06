4863889113a8e457a999b2a51e1e173e94c463e4
baranan <baranan@tauex.tau.ac.il>
2024-02-06 09:52:18 +0000

    debug.true for a few minutes

--- docs/studies/datapipe.example.iat1/mgr.js
+++ docs/studies/datapipe.example.iat1/mgr.js
@@ -12,7 +12,7 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv', debug:true});	
+	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
